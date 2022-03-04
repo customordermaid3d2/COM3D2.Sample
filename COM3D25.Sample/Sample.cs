d0607b55fd9b9234ae5a2690b3fc5e686cdcbe67
lilly <lilly1987@naver.com>
2022-03-04 10:13:25 +0900

    GUI추가

--- COM3D25.Sample/Sample.cs
+++ COM3D25.Sample/Sample.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace COM3D2.Sample
 {
@@ -15,14 +16,36 @@ namespace COM3D2.Sample
 
     // 버전 규칙 잇음. 반드시 2~4개의 숫자구성으로 해야함. 미준수시 못읽어들임
     [BepInPlugin(
-        MyAttribute.PLAGIN_FULL_NAME, 
-        MyAttribute.PLAGIN_NAME, 
+        MyAttribute.PLAGIN_FULL_NAME,
+        MyAttribute.PLAGIN_NAME,
         MyAttribute.PLAGIN_VERSION)]
     public class Sample : BaseUnityPlugin
     {
+        private Rect rect;
+        private int winNum=10;
+        private Vector2 scrollPosition;
+
         private void Awake()
         {
             Logger.LogMessage("Awake");
+            rect = new Rect(10, 10, 100, 300);
+        }
+
+        private void OnGUI()
+        {
+            rect = GUILayout.Window(winNum, rect, WindowFunction, "Sample");
+        }
+
+        private void WindowFunction(int id)
+        {
+            GUI.enabled = true;
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+            if (GUILayout.Button("test"))
+            {
+                Logger.LogMessage("test");
+            }
+            GUILayout.EndScrollView();
+            GUI.DragWindow(); // 창을 잡고 이동 가능            
         }
     }
 }
